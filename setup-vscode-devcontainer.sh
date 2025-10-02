5ac20d552e692da52ea1380627802da8ba256d43
ytakhs <21127627+ytakhs@users.noreply.github.com>
2025-10-02 23:25:44 +0900

    fix

--- setup-vscode-devcontainer.sh
+++ setup-vscode-devcontainer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if command -v nix >/dev/null 2>&1; then
-  nix --extra-experimental-features nix-command --extra-experimental-features flakes run nixpkgs#home-manager/master -- init --switch --flake $(HOME)/dotfiles/env/aarch64-vscode-devcontainer#vscode
+  mkdir -p ~/.config/nix
+  echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
+  nix run --extra-experimental-features 'nix-command flakes' github:nix-community/home-manager/master -- switch --flake ~/dotfiles/env/aarch64-vscode-devcontainer#vscode -b backup
 else
   echo "Nix is not installed"
 fi
