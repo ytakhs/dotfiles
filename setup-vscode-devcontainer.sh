8c77121b0070ee99449eb9dacda94e5ea2f60666
ytakhs <21127627+ytakhs@users.noreply.github.com>
2025-10-02 22:55:59 +0900

    fix

--- setup-vscode-devcontainer.sh
+++ setup-vscode-devcontainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ which nix >/dev/null 2>&1 ]; then
+if command -v nix >/dev/null 2>&1; then
   nix run nixpkgs#home-manager/master -- init --switch --flake $(HOME)/dotfiles/env/aarch64-darwin-vscode#vscode
 else
   echo "Nix is not installed"
