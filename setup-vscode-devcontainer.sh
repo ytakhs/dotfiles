d202859d48e9a4828cb4ec3b04ee0c56c120aa21
ytakhs <21127627+ytakhs@users.noreply.github.com>
2026-01-13 10:06:30 +0900

    remove script

--- setup-vscode-devcontainer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if command -v nix >/dev/null 2>&1; then
-  mkdir -p ~/.config/nix
-  echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
-  nix run github:nix-community/home-manager/master -- switch --flake ~/dotfiles/env/aarch64-vscode-devcontainer#vscode -b backup
-else
-  echo "Nix is not installed"
-fi
