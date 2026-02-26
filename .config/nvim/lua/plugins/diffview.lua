1d4a76fc71be9cdae9ddeb5b3a2997706ac1f521
ytakhs <21127627+ytakhs@users.noreply.github.com>
2026-02-26 23:25:50 +0900

    remove diffview from neovim

--- .config/nvim/lua/plugins/diffview.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"sindrets/diffview.nvim",
-	event = "VeryLazy",
-	opts = {},
-	keys = {
-		{ "<leader>do", ":DiffviewOpen<CR>", desc = "Open Diffview" },
-		{ "<leader>dc", ":DiffviewClose<CR>", desc = "Close Diffview" },
-	},
-}
